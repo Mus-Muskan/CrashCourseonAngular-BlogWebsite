a9f7c83f13ee0ff98669bc7d831bdafd9b020e5b
Muskan <muskan.xyz1151@gmail.com>
2023-07-12 07:37:05 -0400

    Day 2 - Introduction to Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    HomeComponent,
+    NavigationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
